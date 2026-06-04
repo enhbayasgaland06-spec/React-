1ed38e9d90d61c11e91bfd8674b64d12a70e2627
enhbayasgaland06-spec <enhbayasgaland06@gmail.com>
2026-06-04 11:44:18 +0800

    react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+"use client"
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [users, SetUsers] = useState([])
+  const [loading, SetLoading] = useState(true)
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/users")
+      const data = await res.json()
+      SetUsers(data)
+      SetLoading(false)
+    }
+    getUsers()
+  }, [])
+
+  if (loading) return <h1>Ачаалалж байна ...</h1>
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-8 bg-gray-100 min-h-screeb">
+      <div className='text-3xl font-bold mb-6'>Хэрэглэгчид</div>
+      {users.map(user => (
+        <div key={user.id} className='bg-white rounded-xl shadow mb-4'>
+          <h2 className='text-xl font-bold text-blue-600'>
+            {user.name}
+          </h2>
+          <p className='text-gray-500'>{user.email}</p>
+          <p className='text-gray-400'>{user.phone}</p>
+        </div>
+      ))}
     </div>
   );
-}
+} 
 
-export default App;
+
